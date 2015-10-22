f96ddd20ddc5fcf9977267c1e5d4b26ce967885d
Martin Duhem <martin.duhem@gmail.com>
2015-10-22 16:30:19 +0200

    Split

--- project1/main.m
+++ project1/main.m
@@ -9,11 +9,14 @@ y = y_train;
 y(y == -1) = 0;
 X = normalize(X);
 
-tX = [ones(length(y), 1) X];
+[XTr, yTr, XTe, yTe] = split(y, X, 0.5);
+
+tXTr = [ones(length(yTr), 1) XTr];
 alpha = 0.1;
 lambda = 1;
-%betaLS = leastSquares(y, tX);
-%betaGD = leastSquaresGD(y, tX, alpha);
-%betaRR = ridgeRegression(y, phi(X, 4), lambda);
-betaLR = logisticRegression(y, tX, alpha);
-betaPLR = penLogisticRegression(y, tX, alpha, lambda);
+betaLS = leastSquares(yTr, tXTr);
+betaGD = leastSquaresGD(yTr, tXTr, alpha);
+betaRR = ridgeRegression(yTr, phi(XTr, 4), lambda);
+betaLR = logisticRegression(yTr, tXTr, alpha);
+betaPLR = penLogisticRegression(yTr, tXTr, alpha, lambda);
+
