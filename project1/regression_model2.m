05ec9a92f892e81f04f526a4ec66b424215afb8f
Allan Renucci <allan.renucci@gmail.com>
2015-10-22 22:12:34 +0200

    vary degree and lambda

--- project1/regression_model2.m
+++ project1/regression_model2.m
@@ -4,8 +4,8 @@ clear all
 load('PuntaCana_regression.mat');
 
 % Constants
-lambda = 1;
-degree = 15;
+lambdas = logspace(-2,2,100);
+degrees = [3 7 12];
 K = 5;
 
 X = X_train;
@@ -22,29 +22,44 @@ for k = 1:K
 	idxCV(k,:) = idx(1+(k-1)*Nk:k*Nk);
 end
 
-for k = 1:K
-    % get k'th subgroup in test, others in train
-    idxTe = idxCV(k,:);
-    idxTr = idxCV([1:k-1 k+1:end],:);
-    idxTr = idxTr(:);
-    yTe = y(idxTe);
-    XTe = X(idxTe,:);
-    yTr = y(idxTr);
-    XTr = X(idxTr,:);
-    
-    % form tX
-    tXTr = phi(XTr, degree);
-	tXTe = phi(XTe, degree);
-    
-    beta = ridgeRegression(yTr, tXTr, lambda);
-    
-	% training and test MSE(INSERT CODE)
-	mseTrSub(k) = sqrt(2*computeCost(yTr,tXTr,beta));
+figure
 
-	% testing MSE using least squares
-	mseTeSub(k) = sqrt(2*computeCost(yTe,tXTe,beta));
+for i = 1:length(degrees)
+    degree = degrees(i);
+    
+    for j = 1:length(lambdas)
+        lambda = lambdas(j);
+
+        for k = 1:K    
+            % get k'th subgroup in test, others in train
+            idxTe = idxCV(k,:);
+            idxTr = idxCV([1:k-1 k+1:end],:);
+            idxTr = idxTr(:);
+            yTe = y(idxTe);
+            XTe = X(idxTe,:);
+            yTr = y(idxTr);
+            XTr = X(idxTr,:);
+
+            % form tX
+            tXTr = phi(XTr, degree);
+            tXTe = phi(XTe, degree);
+
+            beta = ridgeRegression(yTr, tXTr, lambda);
+
+            % training and test MSE(INSERT CODE)
+            mseTrSub(k) = sqrt(2*computeCost(yTr,tXTr,beta));
+
+            % testing MSE using least squares
+            mseTeSub(k) = sqrt(2*computeCost(yTe,tXTe,beta));
+        end
+
+        mseTr(j) = mean(mseTrSub);
+        mseTe(j) = mean(mseTeSub);
+    end
+
+    % plot
+    subplot(length(degrees), 1, i);
+    semilogx(lambdas, mseTr, lambdas, mseTe);
+    
 end
-
-mseTr = mean(mseTrSub);
-mseTe = mean(mseTeSub);
 
