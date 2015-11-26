f453a3fcf1fe6760e1becc7c740527bbc28d11dd
Martin Duhem <martin.duhem@gmail.com>
2015-11-26 15:34:14 +0100

    Linear kernel

--- lab7/linear_kernel.m
+++ lab7/linear_kernel.m
@@ -1,7 +1,5 @@
 function [ K ] = linear_kernel( X1, X2 )
-%LINEAR_KERNEL Build a linear kernel.
-
-% TODO: implement the linear kernel
-
+    %LINEAR_KERNEL Build a linear kernel.
+    K = phi(X1) * phi(X2)';
 end
 
