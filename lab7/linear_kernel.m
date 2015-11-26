9aa7f8481b828bb0b706cd080a423af671982ee7
Martin Duhem <martin.duhem@gmail.com>
2015-11-26 14:26:46 +0100

    lab7

---
+++ lab7/linear_kernel.m
@@ -0,0 +1,7 @@
+function [ K ] = linear_kernel( X1, X2 )
+%LINEAR_KERNEL Build a linear kernel.
+
+% TODO: implement the linear kernel
+
+end
+
---
+++ lab7/rbf_kernel.m
@@ -0,0 +1,7 @@
+function [ K ] = rbf_kernel( X1, X2,  gamma)
+%RBF_KERNEL Compute the radial basis function kernel matrix.
+
+% TODO: implement the RBF kernel
+
+end
+
