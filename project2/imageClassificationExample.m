b0e24e8dabd4e641e3471de2fa4676fad7c8a81d
Martin Duhem <martin.duhem@gmail.com>
2015-12-09 16:21:25 +0100

    reduceDimension in example

--- project2/imageClassificationExample.m
+++ project2/imageClassificationExample.m
@@ -31,12 +31,15 @@ Tr = [];
 Te = [];
 
 % NOTE: you should do this randomly! and k-fold!
-Tr.idxs = 1:2:size(train.X_hog,1);
-Tr.X = train.X_hog(Tr.idxs,:);
+
+Tr.concat = train.X_cnn; %[normalize2(train.X_hog) * reduceDimension(normalize2(train.X_hog))];
+
+Tr.idxs = 1:2:size(Tr.concat,1);
+Tr.X = Tr.concat(Tr.idxs,:);
 Tr.y = train.y(Tr.idxs);
 
-Te.idxs = 2:2:size(train.X_hog,1);
-Te.X = train.X_hog(Te.idxs,:);
+Te.idxs = 2:2:size(Tr.concat,1);
+Te.X = Tr.concat(Te.idxs,:);
 Te.y = train.y(Te.idxs);
 
 %%
@@ -49,14 +52,12 @@ rng(8339);  % fix seed, this    NN may be very sensitive to initialization
 
 % setup NN. The first layer needs to have number of features neurons,
 %  and the last layer the number of classes (here four).
-nn = nnsetup([size(Tr.X,2) 10 4]);
 opts.numepochs =  20;   %  Number of full sweeps through data
 opts.batchsize = 100;  %  Take a mean gradient step over this many samples
 
 % if == 1 => plots trainin error as the NN is trained
 opts.plot               = 1;
 
-nn.learningRate = 2;
 
 % this neural network implementation requires number of samples to be a
 % multiple of batchsize, so we remove some for this to be true.
@@ -66,6 +67,11 @@ Tr.y = Tr.y(1:numSampToUse);
 
 % normalize data
 [Tr.normX, mu, sigma] = zscore(Tr.X); % train, get mu and std
+Tl = reduceDimension(Tr.normX);
+Tr.normX = Tr.normX * Tl;
+
+Te.normX = normalize(Te.X, mu, sigma);  % normalize test data
+Te.normX = Te.normX * Tl;
 
 % prepare labels for NN
 LL = [1*(Tr.y == 1), ...
@@ -74,10 +80,9 @@ LL = [1*(Tr.y == 1), ...
       1*(Tr.y == 4) ];  % first column, p(y=1)
                         % second column, p(y=2), etc
 
+nn = nnsetup([size(Tr.normX,2) 10 4]);
+nn.learningRate = 2;                        
 [nn, L] = nntrain(nn, Tr.normX, LL, opts);
-
-
-Te.normX = normalize(Te.X, mu, sigma);  % normalize test data
 
 % to get the scores we need to do nnff (feed-forward)
 %  see for example nnpredict().
@@ -96,8 +101,9 @@ nnPred = nn.a{end};
 % get overall error [NOTE!! this is not the BER, you have to write the code
 %                    to compute the BER!]
 predErr = sum( classVote ~= Te.y ) / length(Te.y);
+ber = BER(4, Te.y, classVote);
 
-fprintf('\nTesting error: %.2f%%\n\n', predErr * 100 );
+fprintf('\nTesting error: %.2f%%, ber=%f\n\n', predErr * 100, ber);
 
 
 %% visualize samples and their predictions (test set)
@@ -110,7 +116,7 @@ for i=20:30  % just 10 of them, though there are thousands
 
 
     % show if it is classified as pos or neg, and true label
-    title(sprintf('Label: %d, Pred: %d', train.y(Te.idxs(i)), classVote(i)));
+    title(sprintf('Label: %s, Pred: %s', labelToString(train.y(Te.idxs(i))), labelToString(classVote(i))));
 
     pause;  % wait for keydo that then,?? 
 end
